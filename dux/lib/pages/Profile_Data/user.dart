04cfd0d534f88bd4d720067468dd2d98bc8c3d28
Diana Siso <diana.siso@ua.pt>
2022-04-08 15:44:25 +0100

    páginas random

---
+++ dux/lib/pages/Profile_Data/user.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+class User {
+  String image;
+  String name;
+  String email;
+  String phone;
+  String aboutMeDescription;
+
+  // Constructor
+  User({
+    required this.image,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.aboutMeDescription,
+  });
+
+  User copy({
+    String? imagePath,
+    String? name,
+    String? phone,
+    String? email,
+    String? about,
+  }) =>
+      User(
+        image: imagePath ?? this.image,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        phone: phone ?? this.phone,
+        aboutMeDescription: about ?? this.aboutMeDescription,
+      );
+
+  static User fromJson(Map<String, dynamic> json) => User(
+        image: json['imagePath'],
+        name: json['name'],
+        email: json['email'],
+        aboutMeDescription: json['about'],
+        phone: json['phone'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'imagePath': image,
+        'name': name,
+        'email': email,
+        'about': aboutMeDescription,
+        'phone': phone,
+      };
+}
+
+class UserData {
+  static late SharedPreferences _preferences;
+  static const _keyUser = 'user';
+
+  static User myUser = User(
+    image:
+        "https://upload.wikimedia.org/wikipedia/en/0/0b/Darth_Vader_in_The_Empire_Strikes_Back.jpg",
+    name: 'Test Test',
+    email: 'test.test@gmail.com',
+    phone: '(208) 206-5039',
+    aboutMeDescription:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...',
+  );
+
+  static Future init() async =>
+      _preferences = await SharedPreferences.getInstance();
+
+  static Future setUser(User user) async {
+    final json = jsonEncode(user.toJson());
+
+    await _preferences.setString(_keyUser, json);
+  }
+
+  static User getUser() {
+    final json = _preferences.getString(_keyUser);
+
+    return json == null ? myUser : User.fromJson(jsonDecode(json));
+  }
+}
---
+++ dux/lib/pages/SecondScreen.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+  
+class SecondScreen extends StatefulWidget {  
+  @override  
+  _ProfilePageState createState() => _ProfilePageState();
+}  
+
+class _ProfilePageState extends State<SecondScreen> {
+  final double coverHeight = 280;
+  final double profileHeight = 144;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.all(30.0),
+        children: <Widget>[
+          buildTop(),
+          buildContent(),
+        ],
+      ), 
+    );
+  }
+
+  Widget buildContent() => Column(
+    children: [
+      const SizedBox(height: 8),
+      Text('Flávio Pereira', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+      const SizedBox(height: 8),
+      Text('Software Engineer', style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.normal)),
+     
+      const SizedBox(height: 16),
+      Divider(),
+      const SizedBox(height: 16),
+    ],
+  );
+  
+  Widget buildTop() {
+    final top = coverHeight - profileHeight/2;
+    final bottom = profileHeight / 2;
+    return Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: bottom),
+            child: buildCoverImage()
+          ),
+          Positioned(
+            top: top,
+            child: buildProfileImage()
+            ),
+        ],
+      );
+  }
+
+  Widget buildCoverImage() => Container( 
+    color: Colors.grey,
+    child: Image.network('https://data.whicdn.com/images/344678427/original.jpg',
+      width: double.infinity,
+      fit: BoxFit.cover,
+      height: coverHeight,
+    ),
+  );
+
+  Widget buildProfileImage() => CircleAvatar(
+    radius: profileHeight / 2,
+    backgroundColor: Colors.white,
+    child: CircleAvatar(
+      backgroundImage: NetworkImage('http://www.mandysam.com/img/random.jpg'),
+      radius: profileHeight / 2 - 10),
+     );
+}
