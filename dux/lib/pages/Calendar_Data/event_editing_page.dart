33ed125257bdbd2f00c6dea61d3ebad880609210
Diana Siso <diana.siso@ua.pt>
2022-05-13 23:43:35 +0100

    final

--- dux/lib/pages/Calendar_Data/event_editing_page.dart
+++ dux/lib/pages/Calendar_Data/event_editing_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:dux/utils.dart';
 import './Map_Data/map_screen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 
 class EventEditingPage extends StatefulWidget {
   final Event? event;
